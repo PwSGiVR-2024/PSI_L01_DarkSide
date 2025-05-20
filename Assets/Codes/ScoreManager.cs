fb28b6cd0bea9f1bb93dc1bfbc23a4792961909d
Karol <karol200235@wp.pl>
2025-05-20 14:47:54 +0200

    TG-56 #done score manager

---
+++ Assets/Codes/ScoreManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public AudioSource collectSound; // Mo¿na przypisaæ dŸwiêk do ka¿dego collectible
+    public Text scoreText;           // UI do aktualizacji punktów
+    public static int score = 0;     // static = wspólne dla wszystkich
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Zbiera tylko gracz
+        {
+            collectSound.Play();
+            score++;
+            scoreText.text = "Score: " + score;
+            gameObject.SetActive(false); // Ukryj collectible
+        }
+    }
+}
