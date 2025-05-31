7c32ce1907243653bf521cf6b5f28f604e3f1fc8
krmateusz <mtuzgithub@gmail.com>
2025-05-31 14:03:52 +0200

    poprawki i bugfixy

---
+++ Assets/Codes/ParallaxBackground.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private float startPos, lenght;
+    public GameObject cam;
+    public float parallaxEffect;
+    void Start()
+    {
+        startPos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > startPos + lenght)
+        {
+            startPos += lenght;
+        }
+        else if (movement < startPos - lenght)
+        {
+            startPos -= lenght;
+        }
+
+
+    }
+
+
+
+
+}
